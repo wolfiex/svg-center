fdc924ceee4186caa9d42108312d77f224a1f18f
Daniel Ellis <daniel.ellis@york.ac.uk>
2018-08-30 17:03:14 +0100

    test

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+/* *
+ * Escape special characters in the given string of html.
+ *
+ * @param  {String} html
+ * @return {String}
+ */
+module.exports = {
+  shrink: function(s) {
+        var btop =[],bbottom =[],bleft=[],bright=[];
+        //var s = document.getElementsByTagName('svg')[3];
+        [...s.children].forEach(c =>{
+        var val = c.getBoundingClientRect();
+        btop.push(val.top);
+        bbottom.push(val.bottom);
+        bleft.push(val.left);
+        bright.push(val.right);
+        })
+
+        btop = Math.min(...btop);
+        bbottom = Math.max(...bbottom);
+        bleft = Math.min(...bleft);
+        bright = Math.max(...bright);
+
+        var bx= Math.abs(btop-bbottom);
+        var by= Math.abs(bleft-bright);
+
+        s.setAttribute('preserveAspectRatio',"none");
+        s.style.width = bx;
+        s.style.height = by;
+        s.setAttribute('viewBox',''+0+' '+0+ ' '+bx+' '+by);
+        s.setAttribute('preserveAspectRatio',"xMinYMin meet");
+        
+        return s 
+  },
+
+
+  shrinkall: function(html) {
+        [...document.getElementsByTagName('svg')].forEach(s=>shrink(s))
+  }
+};
