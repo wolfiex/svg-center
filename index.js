72fade449495d83c20832612f13251aea1119841
Dan Ellis <daniel.ellis.research@googlemail.com>
2018-09-01 21:40:50 +0100

    viewbox fix

--- index.js
+++ index.js
@@ -5,12 +5,14 @@
  * @param  {String} html
  * @return {String}
  */
- 
+
  function shrink(s) {
        var btop =[],bbottom =[],bleft=[],bright=[];
        //var s = document.getElementsByTagName('svg')[3];
        [...s.children].forEach(c =>{
        var val = c.getBoundingClientRect();
+
+       // filter points not on image
        btop.push(val.top);
        bbottom.push(val.bottom);
        bleft.push(val.left);
@@ -22,16 +24,21 @@
        bleft = Math.min(...bleft);
        bright = Math.max(...bright);
 
+
        var bx= Math.abs(btop-bbottom);
        var by= Math.abs(bleft-bright);
 
+
+              //btop = (btop <0)?0:btop;
+              //bleft = (bleft <0)?0:bleft;
+console.log(bleft,bright,bbottom,btop,bx,by)
        s.setAttribute('preserveAspectRatio',"none");
        s.style.width = bx;
        s.style.height = by;
-       s.setAttribute('viewBox',''+0+' '+0+ ' '+bx+' '+by);
+       s.setAttribute('viewBox',''+bleft/s.clientWidth+' '+btop/s.clientHeight+ ' '+bx+' '+by);
        s.setAttribute('preserveAspectRatio',"xMinYMin meet");
-       
-       return s 
+
+       return s
  }
 module.exports = {
   shrink: shrink,
